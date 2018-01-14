8f0a02f55c8aae97a4cc1d300f9bc4b80ff19a1d
chaewon-lee <chaewon.lee1116@gmail.com>
2018-01-15 06:11:17 +0900

    fix market activity

--- app/src/main/java/com/moco/wonchu/iwantyoucook/BookActivity.java
+++ app/src/main/java/com/moco/wonchu/iwantyoucook/BookActivity.java
@@ -1,7 +1,9 @@
 package com.moco.wonchu.iwantyoucook;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -24,5 +26,41 @@ public class BookActivity extends AppCompatActivity {
         TextView asitext= (TextView)findViewById(R.id.asianText);
         TextView westext= (TextView)findViewById(R.id.westernText);
         TextView favtext= (TextView)findViewById(R.id.favoriteText);
+
+        korean.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent= new Intent(BookActivity.this, ListActivity.class);
+                intent.putExtra("BUTTON_ID", 1);
+                startActivity(intent);
+            }
+        });
+
+        asian.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent= new Intent(BookActivity.this, ListActivity.class);
+                intent.putExtra("BUTTON_ID", 2);
+                startActivity(intent);
+            }
+        });
+
+        western.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent= new Intent(BookActivity.this, ListActivity.class);
+                intent.putExtra("BUTTON_ID", 3);
+                startActivity(intent);
+            }
+        });
+
+        favorite.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent= new Intent(BookActivity.this, ListActivity.class);
+                intent.putExtra("BUTTON_ID", 4);
+                startActivity(intent);
+            }
+        });
     }
 }
